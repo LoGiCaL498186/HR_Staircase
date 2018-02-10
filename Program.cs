1600a45a7d50b4e58f5c42b8e85f1b80f3e09931
dave498186 <dave498186@gmail.com>
2018-02-10 02:50:39 -0500

    Staircase

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void staircase(int n)
+    {
+        // Complete this function
+        string tempLine = string.Empty;
+        for (int i = 1;  i <= n; i++)
+        {
+            tempLine = new string(' ', n - i) + new string('#', i);
+            Console.WriteLine(tempLine);
+
+        }
+
+        Console.ReadKey();
+
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        staircase(n);
+    }
+}
